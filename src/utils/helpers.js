28f535acb94c696a0b8767d66f5196bd92ca8c26
jfalxa <jfalxa.sta@gmail.com>
2018-05-04 18:07:26 +0200

    Better approach to selection toggle

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,30 +1,44 @@
-import xorWith from 'lodash/xorWith'
+import xor from 'lodash/xor'
 import uniq from 'lodash/uniq'
-import startsWith from 'lodash/startsWith'
+import uniqBy from 'lodash/uniqBy'
 import * as Tree from './tree'
 import { bbox, joinBboxes } from './geometry'
 import { transform } from '../shapes'
 
 
-const comparePaths = (a, b) => (startsWith(a, b) || startsWith(b, a))
+const getAncestors = (tree, elements) => uniq(elements.map(id => Tree.findAncestor(tree, { id }).id))
+const getPaths = (tree, elements) => elements.map(id => Tree.findPath(tree, { id }))
 
 
-// this solution is not great, it behaves weirdly when combining subselection and toggle
 export function selectElements(state, selection) {
-  const newSelection = !selection.subselection
-    ? uniq(selection.elements.map(id => Tree.findAncestor(state.elements, { id }).id))
-    : selection.elements
-
-  if (selection.toggle) {
-    const selectionPaths = state.selection.map(id => Tree.findPath(state.elements, { id }))
-    const newSelectionPaths = newSelection.map(id => Tree.findPath(state.elements, { id }))
-
-    return xorWith(selectionPaths, newSelectionPaths, comparePaths)
-      .map(path => Tree.at(state.elements, path))
-      .map(element => element.id)
+  // basic case: user clicks on an element in the layer tree
+  if (!selection.toggle && selection.subselection) {
+    return selection.elements
   }
 
-  return newSelection
+  // user clicks on one element on the stage
+  else if (!selection.toggle && !selection.subselection) {
+    return getAncestors(state.elements, selection.elements)
+  }
+
+  // user shift-clicks on layer tree
+  else if (selection.toggle && selection.subselection) {
+    return xor(state.selection, selection.elements)
+  }
+
+  // user shift-clicks on the stage
+  else if (selection.toggle && !selection.subselection) {
+    const currentPaths = getPaths(state.elements, state.selection)
+    const newPaths = getPaths(state.elements, selection.elements)
+
+    // only keep the paths that are exclusive to each selection array in order
+    // to allow users to remove elements from selection by shift clicking them
+    const remainingCurrentPaths = currentPaths.filter(currentPath => !newPaths.some(newPath => String(newPath).startsWith(currentPath)))
+    const remainingNewPaths = newPaths.filter(newPath => !currentPaths.some(currentPath => String(newPath).startsWith(currentPath))).map(path => [path[0]])
+
+    return uniqBy([...remainingCurrentPaths, ...remainingNewPaths], String)
+      .map(path => Tree.at(state.elements, path).id)
+  }
 }
 
 export function getSelectionElements({ selection, elements }) {
